e5259c8210a21dfdf55176a4075c36e79a13f032
dgrabo <grabovacdado@gmail.com>
2024-06-16 16:52:22 +0200

    looping over movies array fetched from api

--- src/App.js
+++ src/App.js
@@ -24,10 +24,11 @@ const App = () => {
     const [movies, setMovies] = useState([]);
 
     const searchMovies = async (title) => {
-        const response = await fetch(`${        API_URL}&s=${title}`);
+        const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
+         
     }
 
     useEffect(() => {
@@ -51,10 +52,25 @@ const App = () => {
                     onClick={() => {}}
                 />
             </div>
+             
 
-            <div className="container">
-                <MovieCard movie1={movie1}/>
-            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie}/>
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movie found</h2>
+                        </div>
+                    )
+
+            }
+
+            
         </div>
     );
 }
