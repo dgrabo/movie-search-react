b4cc9bf581ea17206ec547788c09487d05b327ed
dgrabo <grabovacdado@gmail.com>
2024-06-18 22:46:16 +0200

    search funcionality

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const movie1 = {
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -43,34 +44,29 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
              
 
-            {
-                movies?.length > 0
-                    ? (
-                        <div className="container">
-                            {movies.map((movie) => (
-                                <MovieCard movie={movie}/>
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="empty">
-                            <h2>No movie found</h2>
-                        </div>
-                    )
-
-            }
-
-            
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movie found</h2>
+                    </div>
+                )}
         </div>
     );
 }
